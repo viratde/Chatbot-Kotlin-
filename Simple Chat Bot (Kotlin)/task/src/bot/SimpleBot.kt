343d2ff25bdcf8deb8afbaf417957ff5e363702d
viratde <virat7107029@gmail.com>
2024-01-10 12:42:01 +0530

    Third Stage Completed

--- Simple Chat Bot (Kotlin)/task/src/bot/SimpleBot.kt
+++ Simple Chat Bot (Kotlin)/task/src/bot/SimpleBot.kt
@@ -10,8 +10,18 @@ fun main() {
     println("I was created in 2020.")
     println("Please, remind me your name.")
 
-    // reading a name
-    val userName = readln()
+    val name = scanner.nextLine()
 
-    println("What a great name you have, $userName!")
+    println("What a great name you have, $name!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    // reading all remainders
+    val rem1 = readln().toInt()
+    val rem2 = readln().toInt()
+    val rem3 = readln().toInt()
+
+    val userAge = (rem1 * 70 + rem2 * 21 + rem3 * 15) % 105
+
+    println("Your age is $userAge; that's a good time to start programming!")
 }
